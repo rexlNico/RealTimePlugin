3c74841638f6e4c11296eeb4da20a84a5804f318
fluse1367 <fluse1367@software4you.eu>
2021-03-07 13:21:51 +0100

    clean-up

--- src/main/java/de/rexlnico/realtimeplugin/commands/Commands.java
+++ src/main/java/de/rexlnico/realtimeplugin/commands/Commands.java
@@ -2,55 +2,56 @@ package de.rexlnico.realtimeplugin.commands;
 
 import de.rexlnico.realtimeplugin.main.Main;
 import de.rexlnico.realtimeplugin.methodes.Messages;
-import de.rexlnico.realtimeplugin.methodes.WeatherWorld;
-import org.bukkit.Bukkit;
+import de.rexlnico.realtimeplugin.methodes.WorldContainer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
-import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Commands implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if(sender instanceof Player){
-            Player player = (Player) sender;
-            if(player.hasPermission("realtime.reload")){
-                if(args.length == 1){
-                    if(args[0].equalsIgnoreCase("reload")){
-                        Main.getWorldManager().loadNew();
-                        Main.getWorldManager().updateAll();
-                        player.sendMessage(Messages.prefix + Messages.reloadAllMSG);
+        if (!sender.hasPermission("realtime.reload")) {
+            sender.sendMessage(Messages.prefix + Messages.noPermissions);
+            return true;
+        }
+        if (args.length > 0) {
+            // TODO: replace with switch-case if more sub-commands get added
+            if (args[0].equalsIgnoreCase("reload")) {
+                if (args.length > 1) {
+                    String file = args[1];
+                    WorldContainer world = Main.getWorldManager().getWeatherWorld(file);
+                    if (world == null) {
+                        sender.sendMessage(Messages.prefix + Messages.noFile.replace("%file%", file));
+                        return false;
                     }
-                }else if(args.length == 2){
-                    if(args[0].equalsIgnoreCase("reload")){
-                        WeatherWorld weatherWorld = Main.getWorldManager().getWeatherWorld(args[1]);
-                        if(weatherWorld == null){
-                            player.sendMessage(Messages.prefix + Messages.noFile.replace("%file%", args[1]));
-                            return false;
-                        }
-                        weatherWorld.update();
-                        player.sendMessage(Messages.prefix + Messages.reloadFileMSG.replace("%file%", args[1]));
-                    }
+                    world.update();
+                    sender.sendMessage(Messages.prefix + Messages.reloadFileMSG.replace("%file%", file));
+                } else {
+                    Main.getWorldManager().loadNew();
+                    Main.getWorldManager().updateAll();
+                    sender.sendMessage(Messages.prefix + Messages.reloadAllMSG);
                 }
+                return true;
             }
         }
+
+        sender.sendMessage(Messages.prefix + Messages.reloadHelp);
         return false;
     }
 
     @Override
     public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] args) {
-        if(args.length == 2){
-            if(args[0].equalsIgnoreCase("reload")){
+        if (args.length == 2) {
+            if (args[0].equalsIgnoreCase("reload")) {
                 return Main.getWorldManager().getTabComplete();
             }
-        }else if(args.length == 1){
-            ArrayList<String> list = new ArrayList<>();
-            list.add("reload");
-            return list;
+        } else if (args.length == 1) {
+            return Collections.singletonList("reload");
         }
 
         return new ArrayList<>();
